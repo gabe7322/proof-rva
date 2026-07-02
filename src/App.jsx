7989f61f060e464582957b15453f3596dc40fd90
gabe7322 <gabe7322@users.noreply.github.com>
2026-07-01 20:55:25 -0700

    feat: TheGap section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero'
+import TheGap from './components/TheGap'
 import './index.css'
 
 export default function App() {
   return (
     <main className="bg-proof-bg">
       <Hero />
+      <TheGap />
     </main>
   )
 }

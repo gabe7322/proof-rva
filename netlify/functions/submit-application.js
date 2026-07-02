a0a4eca0b2ad56699ca63226f272edd6f56c2952
gabe7322 <gabe7322@users.noreply.github.com>
2026-07-02 08:43:44 -0700

    chore: remove debug logging

--- netlify/functions/submit-application.js
+++ netlify/functions/submit-application.js
@@ -12,8 +12,6 @@ export default async function handler(req) {
     })
   }
 
-  console.log('BASE_ID:', process.env.AIRTABLE_BASE_ID)
-  console.log('KEY set:', !!process.env.AIRTABLE_API_KEY)
   const airtableRes = await fetch(
     `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Applications`,
     {
@@ -41,8 +39,6 @@ export default async function handler(req) {
   )
 
   if (!airtableRes.ok) {
-    const airtableError = await airtableRes.json().catch(() => ({}))
-    console.error('Airtable error:', JSON.stringify(airtableError))
     return new Response(JSON.stringify({ error: 'Failed to save application' }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
